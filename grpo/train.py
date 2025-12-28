1e7e504077b08adbdb831f1fa4a6a6812c7539f5
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-12-28 15:02:18 +0000

    unwanted space

--- grpo/train.py
+++ grpo/train.py
@@ -17,7 +17,7 @@ from .buffer import Experience, ReplayBuffer, join_experiences_batch
 SYSTEM_PROMPT = """A conversation between User and Assistant. The user asks a question, and the Assistant solves it.
 The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
 The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively:
- <think> reasoning process here </think>
+<think> reasoning process here </think>
 <answer> answer here </answer>
 """
 
